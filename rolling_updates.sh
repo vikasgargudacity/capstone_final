66d17889029ba6feb8b7d81f11123245b89dd7f1
vikasgargudacity <87815522+vikasgargudacity@users.noreply.github.com>
2021-09-06 18:23:50 +0530

    Update rolling_updates.sh

--- rolling_updates.sh
+++ rolling_updates.sh
@@ -1,7 +1,7 @@
 
 #!/usr/bin/env bash
 DEPLOYMENT_NAME=capstone-proj
-IMAGE_NAME=registry.hub.docker.com/anupshar/udacity-proj4:latest
+IMAGE_NAME=registry.hub.docker.com/vikasgarg0385/capstone-proj:latest
 kubectl get deployments -o wide
 kubectl set image deployments/${DEPLOYMENT_NAME} \
 	${DEPLOYMENT_NAME}=${IMAGE_NAME}
