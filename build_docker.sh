3c92e46c7783985204e97f290b7c529197d5fdde
vikasgargudacity <87815522+vikasgargudacity@users.noreply.github.com>
2021-09-07 21:11:30 +0530

    Update build_docker.sh

--- build_docker.sh
+++ build_docker.sh
@@ -1,5 +1,9 @@
+#!/usr/bin/env bash
+
+# Step 1
 # Build image and add a descriptive tag
 sudo docker build --tag=capstone-proj .
 
 # Step 2
+# Set the container port
 sudo docker run -p 8000:80 capstone-proj
