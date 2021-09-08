7937a51a98ae0170372fb0fe8496bd3f16299253
vikasgargudacity <87815522+vikasgargudacity@users.noreply.github.com>
2021-09-08 19:43:49 +0530

    Update upload_docker.sh

--- upload_docker.sh
+++ upload_docker.sh
@@ -2,16 +2,17 @@
 # This file tags and uploads an image to Docker Hub
 
 # Assumes that an image is built via `build_docker.sh`
+DOCKER_IMAGE="capstone-proj:latest"
 
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-dockerpath=vikasgarg0385/capstone-proj
+dockerpath=vikasgarg0385/${DOCKER_IMAGE}
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
-sudo docker tag capstone-proj $dockerpath
+sudo docker tag ${DOCKER_IMAGE} $dockerpath
 sudo docker login -u "vikasgarg0385" docker.io
 
 # Step 3:
