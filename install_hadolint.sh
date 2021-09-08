b5a6d1ee5113972c296dc59e536231a7ef548adc
vikasgargudacity <87815522+vikasgargudacity@users.noreply.github.com>
2021-09-09 00:12:02 +0530

    Update install_hadolint.sh

--- install_hadolint.sh
+++ install_hadolint.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-sudo wget -O /bin/hadolint https://github.com/hadolint/hadolint/releases/download/v1.16.3/hadolint-Linux-x86_64
-sudo chmod +x /bin/hadolint
+wget -O /bin/hadolint https://github.com/hadolint/hadolint/releases/download/v1.16.3/hadolint-Linux-x86_64
+chmod +x /bin/hadolint
 
