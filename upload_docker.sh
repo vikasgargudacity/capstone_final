0c23d6464e91abc2cadc90be4dec658d14327126
vikasgargudacity <87815522+vikasgargudacity@users.noreply.github.com>
2021-09-08 23:13:42 +0530

    Update upload_docker.sh

--- upload_docker.sh
+++ upload_docker.sh
@@ -13,7 +13,7 @@ dockerpath=vikasgarg0385/${DOCKER_IMAGE}
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 sudo docker tag ${DOCKER_IMAGE} $dockerpath
-sudo docker login -u "vikasgarg0385" docker.io
+sudo docker login -u "$DOCKERHUB_USERID" -p $DOCKERHUB_PASSWORD docker.io
 
 # Step 3:
 # Push image to a docker repository
