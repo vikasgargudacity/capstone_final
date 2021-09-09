3eb90b4ac48fd9c21c4c84f23c5edb2ca6741c0b
vikasgargudacity <87815522+vikasgargudacity@users.noreply.github.com>
2021-09-09 17:25:55 +0530

    Update build_docker.sh

--- build_docker.sh
+++ build_docker.sh
@@ -2,8 +2,8 @@
 
 # Step 1
 # Build image and add a descriptive tag
-sudo docker build --tag=$IMAGE_NAME:$IMAGE_VERSION .
+sudo docker build --tag=${IMAGE_NAME}:${IMAGE_VERSION} .
 
 # Step 2
 # Set the container port
-sudo docker run -t --rm -p 80:80 $IMAGE_NAME:$IMAGE_VERSION
+sudo docker run -t --rm -p 80:80 ${IMAGE_NAME}:${IMAGE_VERSION}
