e74114385fe41245d0222cf70a440d8dd6831cc5
vikasgargudacity <87815522+vikasgargudacity@users.noreply.github.com>
2021-09-09 16:27:42 +0530

    Update build_docker.sh

--- build_docker.sh
+++ build_docker.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
-DOCKER_IMAGE="capstone-project:V2.0"
 
 # Step 1
 # Build image and add a descriptive tag
-sudo docker build --tag=${DOCKER_IMAGE} .
+sudo docker build --tag=$IMAGE_NAME:$IMAGE_VERSION .
 
 # Step 2
 # Set the container port
-sudo docker run -t --rm -p 80:80 ${DOCKER_IMAGE}
+sudo docker run -t --rm -p 80:80 $IMAGE_NAME:$IMAGE_VERSION
