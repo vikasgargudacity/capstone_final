3fbbda9f40e36b7d95f479091823223e731d185e
vikasgargudacity <87815522+vikasgargudacity@users.noreply.github.com>
2021-09-09 17:27:55 +0530

    Delete delete_eks_cluster.sh

--- delete_eks_cluster.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-CLUSTER="capstone-proj"
-REGION="us-west-2"
-CLUSTER_NAME="${CLUSTER}.${REGION}.eksctl.io"
-
-eksctl delete cluster --name "${CLUSTER}" \
-		--region "${REGION_NAME}"
-
